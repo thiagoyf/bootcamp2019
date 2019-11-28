84ae889e60270d41bbc983c37a6183e4b769554f
Thiago Yudi Fukunaga <thiago.fukunaga@soluevo.com.br>
2019-11-28 13:11:55 -0400

    Adicionando as rotas de CRUD de usuário no modulo 01

--- modulo01/index.js
+++ modulo01/index.js
@@ -2,8 +2,44 @@ const express = require("express");
 
 const server = express();
 
-server.get("/teste", (req, res) => {
-  return res.json({ message: "Hello World" });
+server.use(express.json());
+
+const users = ['Diego', 'Robson', 'Victor'];
+
+server.get('/users', (req, res) => {
+
+  return res.json(users);
+});
+
+server.get('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  return res.json(users[index]);
+});
+
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+server.put('/users/:index', (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.send();
 });
 
 server.listen(3000);
